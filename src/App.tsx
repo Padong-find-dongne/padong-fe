5487687cc12854ecee3f90650b703aea47a40e2c
dohee <rhehgml00@naver.com>
2025-04-09 15:34:16 +0900

    feat:이용가이드 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./page/Main";
+import HouseInfo from "./components/HouseInfo";
+import UseGuide from "./page/UseGuide";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/house" element={<HouseInfo />} />
+        <Route path="/guide" element={<UseGuide />} />
       </Routes>
     </Router>
   );
---
+++ src/page/UseGuide.tsx
@@ -0,0 +1,18 @@
+import guide from "../../public/images/guide.png";
+import Header from "../components/Header";
+const UseGuide = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5 flex flex-col items-center justify-center">
+        <span className="p-7 text-[#2e58e4] text-4xl font-bold">
+          파동 <span className="text-[#333333]">이용가이드</span>
+        </span>
+
+        <img className="h-600" src={guide} alt="파동 이용 가이드" />
+      </div>
+    </div>
+  );
+};
+
+export default UseGuide;

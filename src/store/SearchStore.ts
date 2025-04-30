4118c1ba567f8622213e41cb207a3c0a6fa7fa57
dohee <rhehgml00@naver.com>
2025-04-30 13:47:01 +0900

    fix:불필요한 상태 제거

--- src/store/SearchStore.ts
+++ src/store/SearchStore.ts
@@ -1,28 +1,38 @@
 import { create } from "zustand";
 
+type Dong = {
+  dongName: string;
+  dongCode: string;
+};
+
 type SearchStore = {
   inputType: string;
   setInputType: (type: string) => void;
 
-  searchValue: string;
-  setSearchValue: (value: string) => void;
+  // option1용
+  singleDestination: Dong | null;
+  setSingleDestination: (dong: Dong | null) => void;
 
-  multiSearchValues: [string, string];
-  setMultiSearchValue: (index: number, value: string) => void;
+  // option2용
+  multiDestinations: [Dong | null, Dong | null];
+  setMultiDestination: (index: number, dong: Dong | null) => void;
 };
 
 export const useSearchStore = create<SearchStore>((set) => ({
   inputType: "option1",
   setInputType: (type) => set({ inputType: type }),
 
-  searchValue: "",
-  setSearchValue: (value) => set({ searchValue: value }),
+  singleDestination: null,
+  setSingleDestination: (dong) => set({ singleDestination: dong || null }),
 
-  multiSearchValues: ["", ""],
-  setMultiSearchValue: (index, value) =>
+  multiDestinations: [null, null],
+  setMultiDestination: (index, dong) =>
     set((state) => {
-      const updatedValues = [...state.multiSearchValues];
-      updatedValues[index] = value;
-      return { multiSearchValues: updatedValues as [string, string] };
+      const updated = [...state.multiDestinations] as [
+        Dong | null,
+        Dong | null
+      ];
+      updated[index] = dong || null;
+      return { multiDestinations: updated };
     }),
 }));

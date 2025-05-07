0bd7db70a561ca7a5dd2aa208aaec7d2e3dc1539
dohee <rhehgml00@naver.com>
2025-05-07 15:24:48 +0900

    build: proxy 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/news": {
-        target: "http://localhost:8080", // 백엔드 주소
+      "/api": {
+        target: "http://3.39.234.97:8080",
         changeOrigin: true,
-        rewrite: (path) => path,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },

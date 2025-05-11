bb012be3aa66f19cd6c90b606855c8cd74345d8e
dohee <rhehgml00@naver.com>
2025-05-12 01:05:29 +0900

    remove:사용하지 않는 store삭제

--- src/store/BoundaryStore.d.ts
+++
@@ -1,7 +0,0 @@
-interface BoundaryStore {
-    boundaries: Record<string, any>;
-    fetchBoundary: (adminDongCode: string) => Promise<any | null>;
-    clearBoundaries: () => void;
-}
-export declare const useBoundaryStore: import("zustand").UseBoundStore<import("zustand").StoreApi<BoundaryStore>>;
-export {};

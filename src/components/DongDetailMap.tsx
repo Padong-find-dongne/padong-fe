bb4db566bb92fea4d66a33ba4485b42cc55d774e
dohee <rhehgml00@naver.com>
2025-05-12 11:28:40 +0900

    style:지도 marker 배포 경로 설정

--- src/components/DongDetailMap.tsx
+++ src/components/DongDetailMap.tsx
@@ -1,3 +1,4 @@
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
@@ -9,7 +10,17 @@ type Coord = {
 type Position = {
   end: Coord;
 };
+
 const DongDetailMap = ({ end }: Position) => {
+  // 기본 아이콘 설정
+  const defaultIcon = new L.Icon({
+    iconUrl: "/images/marker.png", // 배포 환경에서 절대 경로 사용
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
   return (
     <MapContainer
       center={end}
@@ -20,7 +31,7 @@ const DongDetailMap = ({ end }: Position) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={end}>
+      <Marker position={end} icon={defaultIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
@@ -28,4 +39,5 @@ const DongDetailMap = ({ end }: Position) => {
     </MapContainer>
   );
 };
+
 export default DongDetailMap;

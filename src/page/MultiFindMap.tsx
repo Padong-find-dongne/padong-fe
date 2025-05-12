65b97bf1bc06f14fcdab61ae9d6532fd314ca414
dohee <rhehgml00@naver.com>
2025-05-12 10:09:08 +0900

    fix:버튼 텍스트 수정

--- src/page/MultiFindMap.tsx
+++ src/page/MultiFindMap.tsx
@@ -82,116 +82,113 @@ const MultiFindMap = () => {
   return (
     <div>
       <Header />
-      {isLoading ? (
-        <LoadingSpinner /> // 로딩 중이면 로딩 스피너 표시
-      ) : (
-        <div className="mt-5 ml-10 mr-10 flex flex-row space-x-10">
-          <aside className="basis-1/2 w-100 flex flex-col">
-            <div className="p-2 w-full max-w-md border rounded-lg border-[#3356CC] border-2">
-              <div className="border-b border-gray-200">
-                <span className="font-[600] mr-2">출근지1</span>
-                <span className="text-lg text-[#818181] mb-0.3 mr-5">|</span>
-                <span className="text-[#818181]">
-                  {multiAddress1?.dongName || ""}
-                </span>
-              </div>
-              <div>
-                <span className="font-[600] mr-2">출근지2</span>
-                <span className="text-lg text-[#818181] mb-0.3 mr-5">|</span>
-                <span className="text-[#818181]">
-                  {multiAddress2?.dongName || ""}
-                </span>
-              </div>
+
+      <div className="mt-5 ml-10 mr-10 flex flex-row space-x-10">
+        <aside className="basis-1/2 w-100 flex flex-col">
+          <div className="p-2 w-full max-w-md border rounded-lg border-[#3356CC] border-2">
+            <div className="border-b border-gray-200">
+              <span className="font-[600] mr-2">출근지1</span>
+              <span className="text-lg text-[#818181] mb-0.3 mr-5">|</span>
+              <span className="text-[#818181]">
+                {multiAddress1?.dongName || ""}
+              </span>
             </div>
-
-            <div className="mt-4 space-x-2">
-              <button
-                className={`px-3 py-1 rounded ${
-                  selectedType === "firstMobility"
-                    ? "bg-[#3356CC] text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => setSelectedType("firstMobility")}
-              >
-                목적지1
-              </button>
-              <button
-                className={`px-3 py-1 rounded ${
-                  selectedType === "secondMobility"
-                    ? "bg-[#3356CC] text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => setSelectedType("secondMobility")}
-              >
-                목적지2
-              </button>
-              <button
-                className={`px-3 py-1 rounded ${
-                  selectedType === "intersectedMobility"
-                    ? "bg-[#3356CC] text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => setSelectedType("intersectedMobility")}
-              >
-                겹치는 곳
-              </button>
+            <div>
+              <span className="font-[600] mr-2">출근지2</span>
+              <span className="text-lg text-[#818181] mb-0.3 mr-5">|</span>
+              <span className="text-[#818181]">
+                {multiAddress2?.dongName || ""}
+              </span>
             </div>
+          </div>
 
-            <div className="mt-6 h-[700px] overflow-y-auto ">
-              {Array.isArray(selectedRecommendations) &&
-              selectedRecommendations.length > 0 ? (
-                selectedRecommendations.map((rec) => (
-                  <div
-                    className="mb-15 mt-3 cursor-pointer"
-                    key={rec.departureDong.adminDongCode}
-                    onClick={handleDong(rec)}
-                  >
-                    <p className="text-[#3D3D3D] font-bold text-[20px]">
-                      {rec.departureDong.address}
-                    </p>
+          <div className="mt-4 space-x-2">
+            <button
+              className={`px-3 py-1 rounded ${
+                selectedType === "firstMobility"
+                  ? "bg-[#3356CC] text-white"
+                  : "bg-gray-200"
+              }`}
+              onClick={() => setSelectedType("firstMobility")}
+            >
+              출근지1
+            </button>
+            <button
+              className={`px-3 py-1 rounded ${
+                selectedType === "secondMobility"
+                  ? "bg-[#3356CC] text-white"
+                  : "bg-gray-200"
+              }`}
+              onClick={() => setSelectedType("secondMobility")}
+            >
+              출근지2
+            </button>
+            <button
+              className={`px-3 py-1 rounded ${
+                selectedType === "intersectedMobility"
+                  ? "bg-[#3356CC] text-white"
+                  : "bg-gray-200"
+              }`}
+              onClick={() => setSelectedType("intersectedMobility")}
+            >
+              겹치는 곳
+            </button>
+          </div>
 
-                    <div className="score mt-3">
-                      <div className="grid grid-cols-2 gap-3">
-                        <div className="flex col-span-2">
-                          <span>파동 점수</span>
-                          <div className="px-2 py-0.5 bg-[#3356CC] border rounded-lg text-xs text-white ml-2">
-                            {rec.score}
-                          </div>
+          <div className="mt-6 h-[700px] overflow-y-auto ">
+            {Array.isArray(selectedRecommendations) &&
+            selectedRecommendations.length > 0 ? (
+              selectedRecommendations.map((rec) => (
+                <div
+                  className="mb-15 mt-3 cursor-pointer"
+                  key={rec.departureDong.adminDongCode}
+                  onClick={handleDong(rec)}
+                >
+                  <p className="text-[#3D3D3D] font-bold text-[20px]">
+                    {rec.departureDong.address}
+                  </p>
+
+                  <div className="score mt-3">
+                    <div className="grid grid-cols-2 gap-3">
+                      <div className="flex col-span-2">
+                        <span>파동 점수</span>
+                        <div className="px-2 py-0.5 bg-[#3356CC] border rounded-lg text-xs text-white ml-2">
+                          {rec.score}
                         </div>
-                        <div>
-                          <span>안전등급</span> {rec.safety}
-                        </div>
-                        <div>
-                          <span>인구밀도</span> {rec.density}
-                        </div>
-                        <div>
-                          <span>평균 월세</span> {rec.avgMonthlyRent}
-                        </div>
-                        <div>
-                          <span>평균 전세</span> {rec.avgJeonseDeposit}
-                        </div>
-                        <div className="col-span-2 mt-2">
-                          <hr className="mb-2 border-gray-200" />
-                          <span>출퇴근 시간</span> {rec.avgTime}
-                        </div>
-                        <div className="col-span-2">
-                          <span>평균 유동인구</span> {rec.totalMobility}
-                        </div>
+                      </div>
+                      <div>
+                        <span>안전등급</span> {rec.safety}
+                      </div>
+                      <div>
+                        <span>인구밀도</span> {rec.density}
+                      </div>
+                      <div>
+                        <span>평균 월세</span> {rec.avgMonthlyRent}
+                      </div>
+                      <div>
+                        <span>평균 전세</span> {rec.avgJeonseDeposit}
+                      </div>
+                      <div className="col-span-2 mt-2">
+                        <hr className="mb-2 border-gray-200" />
+                        <span>출퇴근 시간</span> {rec.avgTime}
+                      </div>
+                      <div className="col-span-2">
+                        <span>평균 유동인구</span> {rec.totalMobility}
                       </div>
                     </div>
                   </div>
-                ))
-              ) : (
-                <p>추천 데이터가 없습니다.</p>
-              )}
-            </div>
-          </aside>
-
-          <div className="basis-1/2">
-            <SeoulMap />
+                </div>
+              ))
+            ) : (
+              <p>추천 데이터가 없습니다.</p>
+            )}
           </div>
+        </aside>
+
+        <div className="basis-1/2">
+          <SeoulMap />
         </div>
-      )}
+      </div>
     </div>
   );
 };
